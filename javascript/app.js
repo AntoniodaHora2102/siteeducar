c66eff8637a0af8f0f37a1e9e5e137e9067560d5
Antonio <antonio_dahora@hotmail.com>
2021-05-25 14:52:32 -0300

    site

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+/* instancia do botao chaveador*/
+var alternador = document.querySelector('.js-botao-chaveador');
+
+alternador.onclick = function () {
+	var menu = document.querySelector('.js-menu');
+	menu.classList.toggle('menu--exibindo');
+}
